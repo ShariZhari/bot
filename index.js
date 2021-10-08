7c89f83aa6b1a60b83efd90f7d79082dfd4dc515
ShariZhari <al227832@edu.uaa.mx>
2021-10-08 16:31:13 -0500

    Heroku config

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 "use strict";
 
-require("dotenv").config();
+require("dotenv").config(); //accede al entorno y trae la conviguracion del archivo .env
 const { Client, Intents } = require("discord.js");
 
-console.log("Here we go again 🕶");
+console.log("Initializing");
+
+const ANSWER = ['Buenas noches', 'Buenos días', 'Buenas tardes']
 
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
-});
+}); //banderas de permisos del bot
 const TOKEN = process.env.TOKEN;
 
 // When the client is ready, run this code (only once)
 client.once("ready", () => {
-  console.log("Ready! 🤖");
+  console.log("We're online!");
 });
+
+function gotMessage(message){
+  //if(message.channel === channelID)
+  if (message.content === 'hola' && !message.author.bot){
+    let randomIndex = Math.floor(Math.random()*ANSWER.length);
+    message.channel.send(ANSWER[randomIndex]);
+  }
+    //message.reply('mundo')
+}
+
+client.on("messageCreate", gotMessage);
 
 // Login to Discord with your client's token
 client.login(TOKEN);
